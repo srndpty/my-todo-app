9572015139301ba68fa7da65a58765187dee53e0
MasakiTakano <lambert_phong@yahoo.co.jp>
2025-09-10 05:22:51 +0900

    fix no cache

--- my-todo-api/entrypoint.sh
+++ my-todo-api/entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-# storage と bootstrap/cache ディレクトリの所有者をwww-dataに変更
-# chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
-chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
+# 本番環境では、まずマイグレーションを実行する
+# （マイグレーションジョブを別に作らない、よりシンプルな方法）
+php artisan migrate --force
 
-# キャッシュをクリア
+# キャッシュをクリアし、再生成する
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# Supervisordを起動
+# Supervisordを起動して、Webサーバを開始
 exec /usr/bin/supervisord -c /etc/supervisord.conf
