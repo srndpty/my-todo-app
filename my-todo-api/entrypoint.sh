965b481409d230b183fd1e28df2c571bcb6c8305
MasakiTakano <lambert_phong@yahoo.co.jp>
2025-09-10 04:25:12 +0900

    fix path

--- my-todo-api/entrypoint.sh
+++ my-todo-api/entrypoint.sh
@@ -4,4 +4,4 @@
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 
 # Supervisordを起動
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+exec /usr/bin/supervisord -c /etc/supervisord.conf
