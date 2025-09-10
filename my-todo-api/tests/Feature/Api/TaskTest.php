75ee392a47fedc3ed9b48abf382485e80e8b6893
MasakiTakano <lambert_phong@yahoo.co.jp>
2025-09-11 07:02:49 +0900

    異常系テストも追加

--- my-todo-api/tests/Feature/Api/TaskTest.php
+++ my-todo-api/tests/Feature/Api/TaskTest.php
@@ -21,6 +21,18 @@ class TaskTest extends TestCase
         $this->postJson('/api/tasks')->assertUnauthorized();
     }
 
+    // 空のタイトルだったら失敗するはず
+    public function test_creating_a_task_requires_a_title(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            // titleを含まない、不正なデータを送信
+            ->postJson('/api/tasks', ['title' => ''])
+            ->assertStatus(422) // ステータスコードが422であることを確認
+            ->assertJsonValidationErrors('title'); // 'title'フィールドでバリデーションエラーが発生したことを確認
+    }
+
     /**
      * 認証済みのユーザーが、自分自身のタスクを取得できることをテスト
      * @return void
@@ -59,6 +71,19 @@ class TaskTest extends TestCase
             'user_id' => $user->id,
         ]);
     }
+
+    /**
+     * 存在しないタスクを更新しようとすると404エラーになることをテスト
+     * @return void
+     */
+    public function test_updating_a_non_existent_task_returns_not_found(): void
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user)
+            ->putJson('/api/tasks/999', ['completed' => true]) // 存在しないID: 999
+            ->assertNotFound(); // ステータスコードが404であることを確認
+    }    
     
     /**
      * 認証済みのユーザーが、他人のタスクを更新できないことをテスト
@@ -72,5 +97,17 @@ class TaskTest extends TestCase
         $this->actingAs($user)
              ->putJson("/api/tasks/{$anotherUserTask->id}", ['completed' => true])
              ->assertForbidden();
+    }
+
+    // 認証済みのユーザーが、他人のタスクを削除できないことをテスト
+    public function test_an_authenticated_user_cannot_delete_others_task(): void
+    {
+        $user = User::factory()->create();
+        $anotherUserTask = Task::factory()->create(); // 別のユーザーのタスクをFactoryで作成
+
+        // $userとして、$anotherUserTaskを削除しようと試みる
+        $this->actingAs($user)
+            ->deleteJson("/api/tasks/{$anotherUserTask->id}")
+            ->assertForbidden(); // ステータスコードが403であることを確認
     }
 }
